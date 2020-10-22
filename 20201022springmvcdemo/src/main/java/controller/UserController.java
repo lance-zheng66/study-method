b02bd24ff34a81abacc04db056f435a0f19e67ad
Bo Zhang <zhangbodut@gmail.com>
2020-10-22 19:07:52 +0800

    Add servlet config to web.xml

--- 20201022springmvcdemo/src/main/java/controller/UserController.java
+++ 20201022springmvcdemo/src/main/java/controller/UserController.java
@@ -1,12 +1,14 @@
 package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
   @RequestMapping("/testString")
+  @ResponseBody
   public String testString(){
     System.out.println("testString方法执行了");
     return "success";
